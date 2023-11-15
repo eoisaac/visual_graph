b7131123fa65787f449efdb1d3ac61dd845e3549
Isaac Santiago <eoisaacc@gmail.com>
2023-11-15 20:58:08 -0300

    chore: install and configure eslint prettier husky and lint-staged

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Ubuntu", "sans-serif"],
+      mono: ["Fira Code", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
