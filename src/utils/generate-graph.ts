e2f24a09355e7c4a33abb48535dd0ade0fdde7a3
Isaac Santiago <eoisaacc@gmail.com>
2024-01-03 18:42:26 -0300

    docs: add code comments

--- src/utils/generate-graph.ts
+++ src/utils/generate-graph.ts
@@ -13,29 +13,29 @@ export const generateGraph = (
   const totalNodes = matrix.length
 
   const nodes = matrix.map((_, index) => {
-    const cIndex = index + 1
-    const RADIUS = 20 * totalNodes
-    const xCoord = Math.cos((cIndex / matrix.length) * 2 * Math.PI) * RADIUS
-    const yCoord = Math.sin((cIndex / matrix.length) * 2 * Math.PI) * RADIUS
+    const cIndex = index + 1 // index começa em 0, mas os vértices começam em 1
+    const RADIUS = 20 * totalNodes // aumenta o raio do círculo de acordo com o número de vértices
+    const xCoord = Math.cos((cIndex / matrix.length) * 2 * Math.PI) * RADIUS // Distribui os vértices em um círculo, eixo x
+    const yCoord = Math.sin((cIndex / matrix.length) * 2 * Math.PI) * RADIUS  // Distribui os vértices em um círculo, eixo y
 
     return {
-      id: `node-${cIndex}`,
-      type: 'circleNode',
-      position: { x: xCoord, y: yCoord },
-      data: { label: String(cIndex), isKruskal },
+      id: `node-${cIndex}`, // id do vértice
+      type: 'circleNode', // tipo do vértice
+      position: { x: xCoord, y: yCoord }, // posição do vértice
+      data: { label: String(cIndex), isKruskal }, // label do vértice e se é um vértice do algoritmo de Kruskal
     }
   })
 
   const edges: Edge[] = []
   matrix.forEach((row, rowIndex) => {
     row.forEach((weight, colIndex) => {
-      const isValidEdge = weight !== 0 && colIndex > rowIndex // Prevent self-loops and duplicate edges
+      const isValidEdge = weight !== 0 && colIndex > rowIndex // previne laços e arestas duplicadas
       if (isValidEdge) {
         edges.push({
-          id: `edge-${rowIndex}-${colIndex}`,
-          source: `node-${rowIndex + 1}`,
-          target: `node-${colIndex + 1}`,
-          data: { label: String(weight), isKruskal },
+          id: `edge-${rowIndex}-${colIndex}`, // id da aresta
+          source: `node-${rowIndex + 1}`, // vértice de origem
+          target: `node-${colIndex + 1}`, // vértice de destino
+          data: { label: String(weight), isKruskal }, // label da aresta e se é uma aresta do algoritmo de Kruskal
         })
       }
     })
--- src/utils/kruskal.ts
+++ src/utils/kruskal.ts
@@ -3,16 +3,17 @@ import { Matrix } from '@/@types/app'
 const createGraph = (verticesCount: number): Matrix => {
   return Array.from({ length: verticesCount }, () =>
     Array(verticesCount).fill(0),
-  )
+  ) // cria uma matriz de adjacência preenchida com zeros
 }
 
+// conversão de matriz de adjacência para lista de adjacência
 const addEdge = (graph: Matrix, u: number, v: number, weight: number) => {
-  graph[u][v] = weight
-  graph[v][u] = weight
+  graph[u][v] = weight // adiciona a aresta na matriz de adjacência
+  graph[v][u] = weight // adiciona a aresta na matriz de adjacência
 }
 
 const find = (parentArray: number[], i: number): number => {
-  return parentArray[i] === i ? i : find(parentArray, parentArray[i])
+  return parentArray[i] === i ? i : find(parentArray, parentArray[i]) // busca recursiva pelo pai do vértice
 }
 
 const union = (
@@ -21,16 +22,16 @@ const union = (
   x: number,
   y: number,
 ) => {
-  const rootX = find(parentArray, x)
-  const rootY = find(parentArray, y)
+  const rootX = find(parentArray, x) // busca o pai do vértice x
+  const rootY = find(parentArray, y) // busca o pai do vértice y
 
-  if (rankArray[rootX] < rankArray[rootY]) {
-    parentArray[rootX] = rootY
-  } else if (rankArray[rootX] > rankArray[rootY]) {
-    parentArray[rootY] = rootX
-  } else {
-    parentArray[rootX] = rootY
-    rankArray[rootY] += 1
+  if (rankArray[rootX] < rankArray[rootY]) { // se a altura da árvore de x for menor que a de y, x é filho de y
+    parentArray[rootX] = rootY // x é filho de y
+  } else if (rankArray[rootX] > rankArray[rootY]) { // se a altura da árvore de x for maior que a de y, y é filho de x
+    parentArray[rootY] = rootX // y é filho de x
+  } else { // se a altura da árvore de x for igual a de y, x é filho de y e a altura da árvore de y aumenta em 1
+    parentArray[rootX] = rootY // x é filho de y
+    rankArray[rootY] += 1 // a altura da árvore de y aumenta em 1
   }
 }
 
@@ -40,41 +41,41 @@ interface KruskalResult {
 }
 
 export const kruskal = (adjacencyMatrix: Matrix): KruskalResult => {
-  const numVertices = adjacencyMatrix.length
-  const edges: Matrix = adjacencyMatrix
+  const numVertices = adjacencyMatrix.length // número de vértices do grafo, que é igual ao número de linhas da matriz de adjacência
+  const edges: Matrix = adjacencyMatrix // cria uma matriz de arestas
     .flatMap((row, i) =>
       row
         .slice(i + 1)
-        .map((weight, j) => (weight !== 0 ? [i, i + 1 + j, weight] : [])),
+        .map((weight, j) => (weight !== 0 ? [i, i + 1 + j, weight] : [])), // cria uma aresta com o peso da matriz de adjacência
     )
-    .sort((a, b) => a[2] - b[2])
+    .sort((a, b) => a[2] - b[2]) // ordena as arestas pelo peso
 
   const result: Matrix = []
   const parent: number[] = Array.from({ length: numVertices }, (_, i) => i)
-  const rank: number[] = Array(numVertices).fill(0)
+  const rank: number[] = Array(numVertices).fill(0) 
 
   let selectedEdges = 0
 
   edges.every(([u, v, weight]) => {
-    const rootU = find(parent, u)
-    const rootV = find(parent, v)
+    const rootU = find(parent, u) // busca o pai do vértice u
+    const rootV = find(parent, v) // busca o pai do vértice v
 
-    if (rootU !== rootV) {
-      result.push([u, v, weight])
-      selectedEdges++
-      union(parent, rank, rootU, rootV)
+    if (rootU !== rootV) { // se os pais forem diferentes, não há ciclo então a aresta é adicionada na árvore geradora mínima
+      result.push([u, v, weight]) // adiciona a aresta na árvore geradora mínima
+      selectedEdges++ // incrementa o número de arestas selecionadas
+      union(parent, rank, rootU, rootV) // une as árvores de u e v
     }
 
-    return selectedEdges < numVertices - 1
+    return selectedEdges < numVertices - 1 // se o número de arestas selecionadas for menor que o número de vértices - 1, continua
   })
 
-  const resultMatrix: Matrix = result.reduce((graph, [u, v, weight]) => {
-    addEdge(graph, u, v, weight)
+  const resultMatrix: Matrix = result.reduce((graph, [u, v, weight]) => { // cria uma matriz de adjacência a partir da árvore geradora mínima
+    addEdge(graph, u, v, weight) // adiciona a aresta na matriz de adjacência
     return graph
-  }, createGraph(numVertices))
+  }, createGraph(numVertices)) // cria uma matriz de adjacência preenchida com zeros
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const totalWeight = result.reduce((acc, [_, __, weight]) => acc + weight, 0)
+  const totalWeight = result.reduce((acc, [_, __, weight]) => acc + weight, 0) // soma os pesos das arestas da árvore geradora mínima
 
-  return { tree: resultMatrix, totalWeight }
+  return { tree: resultMatrix, totalWeight } // retorna a árvore geradora mínima e o peso total dela
 }
